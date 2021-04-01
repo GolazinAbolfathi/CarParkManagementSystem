70b758df947be8c09df8adf3c933ec6af8c40eb3
golazin <abolfathig@student.douglascollege.ca>
2021-03-31 17:19:02 -0700

    "controller edited"

--- CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/controller/Controller_Class.java
+++ CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/controller/Controller_Class.java
@@ -6,62 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-//@RequestMapping("/users")
 public class Controller_Class {
 
-    User user = new User();
-
-    @GetMapping("/")
-    public String greetingForm(Model model) {
-        model.addAttribute("signInTag", user);
-        return "signIn";
+    @RequestMapping("/")
+    public String homePage(Model model) {
+        return "home";
     }
-
-    @PostMapping("/signInTag")
-    public String greetingSubmit(@ModelAttribute User user, Model model) {
-        model.addAttribute("signInTag", user);
-        return "result";
-    }
-//    @RequestMapping("/")
-//    public String setData(Model model)
-//    {
-////        User user=new User();
-////        model.addAttribute(user.first_name,"message");
-//        model.addAttribute("message","Change message");
-//        return "signIn";
-//    }
-//
-//    @GetMapping("/")
-//    public String getData(Model model)
-//    {
-//        User user=new User();
-//        model.addAttribute("greeting",user);
-//        return "greeting";
-//    }
-//
-//    @GetMapping("/greeting")
-//    public String greetingForm(Model model) {
-//        model.addAttribute("greeting", new Greeting());
-//        return "signIn";
-//    }
-
-//    @Autowired
-//    private UserService userService;
-
-    //    @GetMapping
-//    public Collection<User> getUser()
-//    {
-//        return userService.getUser();
-//    }
-
-
-
-//    @PostMapping()
-//    public User postUser(@RequestBody User temp)
-//    {
-//        temp=user;
-//        return userService.createUser(temp);
-//    }
 
     //https://spring.io/guides/gs/handling-form-submission/
 
--- CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/controller/SignInController.java
+++ CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/controller/SignInController.java
@@ -1,15 +1,47 @@
 package com.example.CarParkManagementSystem.controller;
 
+import com.example.CarParkManagementSystem.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SignInController {
-    @RequestMapping("/signIn")
-    public  String getHomePage(@ModelAttribute("user") Model model){
+
+    User user = new User();
+
+    @GetMapping("/signIn")
+    public String greetingForm(Model model) {
+        model.addAttribute("signInTag", user);
         return "signIn";
     }
+
+    @PostMapping("/signInTag")
+    public String greetingSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("signInTag", user);
+        return "result";
+    }
+
+
+//    @Autowired
+//    private UserService userService;
+
+    //    @GetMapping
+//    public Collection<User> getUser()
+//    {
+//        return userService.getUser();
+//    }
+
+
+
+//    @PostMapping()
+//    public User postUser(@RequestBody User temp)
+//    {
+//        temp=user;
+//        return userService.createUser(temp);
+//    }
 
 }
