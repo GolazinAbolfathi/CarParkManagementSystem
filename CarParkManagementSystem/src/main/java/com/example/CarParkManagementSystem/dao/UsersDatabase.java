52b2acf426f8cfdced65202084c98e1c38cbfbf8
Phoredome <Bosco.Li.97@hotmail.com>
2021-04-07 15:44:32 -0700

    Some more descriptive comments

--- CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/dao/UsersDatabase.java
+++ CarParkManagementSystem/src/main/java/com/example/CarParkManagementSystem/dao/UsersDatabase.java
@@ -4,8 +4,13 @@ package com.example.CarParkManagementSystem.dao;
 import com.example.CarParkManagementSystem.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+// MongoRepository is annotated with @Repository already
 public interface UsersDatabase extends MongoRepository<User, String> {
     // Basic methods are pre-defined in MongoRepository
-    // Extras can be added here
+    // Extras can be added here following the auto-generation
+    // naming restrictions.
     User getUserByUsernameAndPassword(String username, String password);
+
+    // If more complex queries are required, an actual implementation
+    // class will need to be created.
 }
