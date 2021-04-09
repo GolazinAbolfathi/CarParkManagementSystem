cda3d2813ead38ad1fba37248531ff3c1bd206fe
golazin <abolfathig@student.douglascollege.ca>
2021-04-08 22:28:47 -0700

    "add deleted test classes"

--- CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
@@ -1,61 +1,16 @@
 package com.example.CarParkManagementSystem.view;
 
-import com.example.CarParkManagementSystem.controller.IParkingController;
-import com.example.CarParkManagementSystem.entity.User;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class SignInDisplayTest {
 
-//    @RunWith(SpringJUnit4ClassRunner.class)
-//    @WebAppConfiguration
-//    @ContextConfiguration(classes = SampleWebApplication.class)
-
-        @Autowired
-        private WebApplicationContext wac;
-
-        private MockMvc mockMvc;
-
-    @Autowired
-    IParkingController controller;
-
-//    @Before
-//    public void setup() {
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-//    }
+    @Test
+    void signInForm() {
+    }
 
     @Test
-    void signInSubmit(@ModelAttribute User user, Model model) {
-
-//        this.mockMvc.perform(get("/signIn/signInTag"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("logInHome"));
-
-
-        //Arrange
-        user.setUsername("eli");
-        user.setPassword("eli");
-        User actual = controller.getUser(user.getUsername(),user.getPassword());
-
-        //Assert
-        assertNotNull(actual);
-
-        //Act
-        model.addAttribute("user", actual);
-
+    void signInSubmit() {
     }
 }
--- CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignUpDisplayTest.java
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignUpDisplayTest.java
@@ -1,58 +1,16 @@
 package com.example.CarParkManagementSystem.view;
 
-import com.example.CarParkManagementSystem.controller.IParkingController;
-import com.example.CarParkManagementSystem.controller.ParkingSystemController;
-import com.example.CarParkManagementSystem.entity.User;
-import org.junit.Test;
-//import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SignUpDisplayTest {
-//    private MockMvc mockMvc;
-
-    public SignUpDisplayTest(){
-
-    }
-    @Autowired
-    private IParkingController iParkingController;
-
-    Model model;
-
-//    @Before
-//    public void setup() {
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-//    }
-//    @Test
-//    void signUpForm() throws Exception {
-//        this.mockMvc.perform(get("/signUpTag"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("signUpResult"));
-//    }
+class SignUpDisplayTest {
 
     @Test
-    public void createNewUser() {
-        // Arrange
-        User user;
-        iParkingController=new ParkingSystemController();
-        user=iParkingController.addUser(
-                "Smatt",
-                "Mithieson",
-                "", "",
-                "",
-                "","",
-                0
-        );
+    void signUpForm() {
+    }
 
-        // Assert
-        assertNotNull(user);
-
-        // Act
-        model.addAttribute("user", user);
+    @Test
+    void createNewUser() {
     }
 }
