a9036585a6c6e23d65eef2f7ef4927d631f19a49
golazin <abolfathig@student.douglascollege.ca>
2021-04-08 23:09:16 -0700

    "add deleted test classes"

--- CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
@@ -1,10 +1,15 @@
 package com.example.CarParkManagementSystem.view;
 
+import com.example.CarParkManagementSystem.controller.IParkingController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignInDisplayTest {
+
+    @Autowired
+    private IParkingController iParkingController;
 
     @Test
     void signInForm() {
--- CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignUpDisplayTest.java
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignUpDisplayTest.java
@@ -1,10 +1,17 @@
 package com.example.CarParkManagementSystem.view;
 
+import com.example.CarParkManagementSystem.controller.IParkingController;
+import com.example.CarParkManagementSystem.controller.ParkingSystemController;
+import com.example.CarParkManagementSystem.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignUpDisplayTest {
+
+    @Autowired
+    private IParkingController iParkingController;
 
     @Test
     void signUpForm() {
@@ -12,5 +19,24 @@ class SignUpDisplayTest {
 
     @Test
     void createNewUser() {
+
+        // Arrange
+        User user;
+        iParkingController=new ParkingSystemController();
+        user=iParkingController.addUser(
+                "Smatt",
+                "Mithieson",
+                "", "",
+                "",
+                "","",
+                0
+        );
+
+        // Assert
+        assertNotNull(user);
+
+        // Act
+//        model.addAttribute("user", user);
+        System.out.println("test works");
     }
 }
