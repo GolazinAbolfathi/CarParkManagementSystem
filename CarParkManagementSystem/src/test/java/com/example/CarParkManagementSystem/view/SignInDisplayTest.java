ea43a1ec35f052ba18b71d9b1aa39ded48a454de
golazin <abolfathig@student.douglascollege.ca>
2021-04-08 23:11:05 -0700

    "add deleted test classes"

--- CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
+++ CarParkManagementSystem/src/test/java/com/example/CarParkManagementSystem/view/SignInDisplayTest.java
@@ -1,12 +1,18 @@
 package com.example.CarParkManagementSystem.view;
 
 import com.example.CarParkManagementSystem.controller.IParkingController;
+import com.example.CarParkManagementSystem.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignInDisplayTest {
+
+    @Autowired
+    IParkingController controller;
 
     @Autowired
     private IParkingController iParkingController;
@@ -16,6 +22,17 @@ class SignInDisplayTest {
     }
 
     @Test
-    void signInSubmit() {
+    void signInSubmit(@ModelAttribute User user, Model model) {
+
+        //Arrange
+        user.setUsername("eli");
+        user.setPassword("eli");
+        User actual = controller.getUser(user.getUsername(),user.getPassword());
+
+        //Assert
+        assertNotNull(actual);
+
+        //Act
+        model.addAttribute("user", actual);
     }
 }
